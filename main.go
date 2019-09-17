016f95325e1c512344b6a99eeb5fdf527fd00b51
zyy <zhangyiyanghz@gmail.com>
2019-09-17 16:24:02 +0800

    aes-ctr

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 
-	encrypt := sb.DesEncrypt([]byte("明文密码"), []byte("12345678"))
+	encrypt := sb.DesEncrypt([]byte("明文密码des-cbc"), []byte("12345678"))
 	decrypt := sb.DesDecrypt(encrypt, []byte("12345678"))
 	fmt.Println(string(decrypt))
 
-	encrypt = sb.Des3Encrypt([]byte("明文密码3"), []byte("123456781234567812345678"))
-
+	encrypt = sb.Des3Encrypt([]byte("明文密码 3des-cbc"), []byte("123456781234567812345678"))
 	decrypt = sb.Des3Decrypt(encrypt, []byte("123456781234567812345678"))
+	fmt.Println(string(decrypt))
 
+	encrypt = sb.AesEncrypt([]byte("明文密码 aes-ctr"), []byte("1234567812345678"))
+	decrypt = sb.AesDecrypt(encrypt, []byte("1234567812345678"))
 	fmt.Println(string(decrypt))
 }
--- 对称密码/aes.go
+++ 对称密码/aes.go
@@ -1,1 +1,45 @@
 package sharePub
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+var iv []byte = []byte("1234567812345678")
+
+func AesEncrypt(plainText, key []byte) []byte {
+	var (
+		cipherText []byte
+		err        error
+		block      cipher.Block
+		stream     cipher.Stream
+	)
+
+	if block, err = aes.NewCipher(key); err != nil {
+		panic(err)
+	}
+
+	stream = cipher.NewCTR(block, iv)
+	cipherText = make([]byte, len(plainText))
+	stream.XORKeyStream(cipherText, plainText)
+
+	return cipherText
+}
+
+func AesDecrypt(cipherText, key []byte) []byte {
+	var (
+		err    error
+		block  cipher.Block
+		stream cipher.Stream
+	)
+
+	if block, err = aes.NewCipher(key); err != nil {
+		panic(err)
+	}
+
+	stream = cipher.NewCTR(block, iv)
+
+	stream.XORKeyStream(cipherText, cipherText)
+
+	return cipherText
+}
