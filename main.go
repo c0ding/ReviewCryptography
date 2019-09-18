e7125c454e3246cad60e4f6ed6b920665db7a12e
zyy <zhangyiyanghz@gmail.com>
2019-09-18 09:52:33 +0800

    rsa

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	sb "github.com/c0ding/ReviewCryptography/对称密码"
+	pc "github.com/c0ding/ReviewCryptography/非对称密码"
 )
 
 func main() {
@@ -18,4 +19,11 @@ func main() {
 	encrypt = sb.AesEncrypt([]byte("明文密码 aes-ctr"), []byte("1234567812345678"))
 	decrypt = sb.AesDecrypt(encrypt, []byte("1234567812345678"))
 	fmt.Println(string(decrypt))
+
+
+	pc.GenerateRsaKey(4096)
+	src := []byte("明文明文明文明文明文明文明文明文明文明文,明,文")
+	cipherText := pc.RsaEncrypt(src, pc.Public)
+	plainText := pc.RsaDecrypt(cipherText, pc.Private)
+	fmt.Println(string(plainText))
 }
---
+++ 非对称密码/rsa.go
@@ -0,0 +1,131 @@
+package pubKeyCrypto
+
+import (
+	"crypto/rsa"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"fmt"
+)
+
+const (
+	Public string =  "public.pem"
+	Private string ="private.pem"
+
+)
+
+// 生成rsa 密钥对，保存到磁盘中
+func GenerateRsaKey(keySize int) {
+	var (
+		privateKey *rsa.PrivateKey
+		err error
+		derText []byte
+		block pem.Block
+		file *os.File
+
+		publicKey rsa.PublicKey
+		derstream []byte
+	)
+
+	if privateKey, err = rsa.GenerateKey(rand.Reader, keySize); err != nil {
+		panic(err)
+	}
+	derText = x509.MarshalPKCS1PrivateKey(privateKey)
+	block = pem.Block{
+		Type:"rsa private key",
+		Bytes:derText,
+	}
+	if file, err = os.Create(Private); err != nil {
+		panic(err)
+	}
+	pem.Encode(file, &block)
+	file.Close()
+
+
+
+
+	publicKey = privateKey.PublicKey
+	if derstream, err = x509.MarshalPKIXPublicKey(&publicKey); err != nil {
+		panic(err)
+	}
+	block = pem.Block{
+		Type:"rsa public key",
+		Bytes:derstream,
+	}
+	if file, err = os.Create(Public); err != nil {
+		panic(err)
+	}
+	pem.Encode(file,&block)
+	file.Close()
+}
+
+func RsaEncrypt(plainText []byte, fileName string) []byte {
+
+	var (
+		file *os.File
+		fileInfo os.FileInfo
+		err error
+		buf []byte
+		block *pem.Block
+		pub interface{}
+		pubKey *rsa.PublicKey
+		cipherText []byte
+		ok bool
+	)
+	if file, err = os.Open(fileName); err != nil {
+		panic(err)
+	}
+
+	if fileInfo, err = file.Stat(); err != nil {
+		panic(err)
+	}
+	buf = make([]byte, fileInfo.Size())
+	file.Read(buf)
+	file.Close()
+	block, _ = pem.Decode(buf)
+
+	if pub, err = x509.ParsePKIXPublicKey(block.Bytes); err != nil {
+		fmt.Println("11111出错")
+		panic(err)
+	}
+	if pubKey, ok = pub.(*rsa.PublicKey); !ok  {
+		fmt.Println("出错")
+		panic(err)
+	}
+	if cipherText, err = rsa.EncryptPKCS1v15(rand.Reader, pubKey, plainText); err != nil {
+		panic(err)
+	}
+	return cipherText
+
+}
+
+func RsaDecrypt(cipherText []byte, fileName string) []byte {
+	var (
+		err error
+		file *os.File
+		fileInfo os.FileInfo
+		buf []byte
+		block *pem.Block
+		privateKey *rsa.PrivateKey
+		plainText []byte
+	)
+
+	if file, err = os.Open(fileName); err != nil {
+		panic(err)
+	}
+	if fileInfo, err = file.Stat(); err != nil {
+		panic(err)
+	}
+	buf = make([]byte, fileInfo.Size())
+	file.Read(buf)
+	file.Close()
+	block, _ = pem.Decode(buf)
+	if privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
+		panic(err)
+	}
+	if plainText, err = rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText); err != nil {
+		panic(err)
+	}
+	return plainText
+}
